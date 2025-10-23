0337a02b2020b3e1c81079b44eb9c164d0568f65
K C <chauhankm81@gmail.com>
2025-10-23 12:19:21 +0530

    Deployment Ready: Finalized Render URL setup

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
+const corsOption = {
+  origin: [
+    'http://localhost:3000',
+    'https://chat-application-henna-five.vercel.app'
+  ],
+  credentials: true
 };
+
 app.use(cors(corsOption));
 
 //routes
